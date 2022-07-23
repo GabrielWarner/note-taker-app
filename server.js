900d6bed4117b8af78d9309fb13ea7a082f9db45
GabrielWarner <gabe0412@uw.edu>
2022-07-22 19:16:50 -0700

    initial file creation

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+//middleware for find static assets
+app.use(express.static("public"));
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const allRoutes = require("./controllers")
+app.use(allRoutes);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log("listenin to port " + PORT);
+});
